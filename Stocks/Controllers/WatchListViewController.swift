df7361057ea3f4f3b5f85a3593d5059939f1ca65
Jason Ou <catopia.app@gmail.com>
2021-07-10 10:58:05 +0800

    Build skeleton code of watchlist view controller.

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -12,8 +12,33 @@ class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setUpSearchController()
     }
 
+    private func setUpSearchController() {
+        let resultVC = SearchResultViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
 
 }
 
+extension WatchListViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultVC = searchController.searchResultsController as? SearchResultViewController,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        // Optimize to reduce number of searches for when user stops typing
+        
+        // Call API to search
+        
+        // Update result controller
+        
+    }
+    
+}
