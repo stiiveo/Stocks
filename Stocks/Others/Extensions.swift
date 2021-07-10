46a6ea7910af6aac637ae304cf29d4608a9337ad
Jason Ou <catopia.app@gmail.com>
2021-07-10 11:11:39 +0800

    Add title view to watchlist VC's navigation bar with custom label.

---
+++ Stocks/Others/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  Stocks
+//
+//  Created by Jason Ou on 2021/7/10.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Framing
+
+extension UIView {
+    var width: CGFloat {
+        frame.size.width
+    }
+    
+    var height: CGFloat {
+        frame.size.height
+    }
+    
+    var left: CGFloat {
+        frame.origin.x
+    }
+    
+    var right: CGFloat {
+        left + width
+    }
+    
+    var top: CGFloat {
+        frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        top + height
+    }
+}
