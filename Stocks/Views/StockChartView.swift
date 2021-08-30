432c47dbb3fb8e36fefc7f638167c9c2593016be
Jason Ou <catopia.app@gmail.com>
2021-08-30 15:38:16 +0800

    Fill gradient color below the price line of the chart view.

--- Stocks/Views/StockChartView.swift
+++ Stocks/Views/StockChartView.swift
@@ -45,47 +45,7 @@ class StockChartView: LineChartView {
         // Switch the appearance of the right and x axis.
         rightAxis.enabled = viewModel.showAxis
         xAxis.enabled = viewModel.showAxis
-        
-        // Chart Data Entries
-        let entries: [ChartDataEntry] = viewModel.data.map{
-            .init(x: $0.timeInterval, y: $0.price)
-        }
-        guard entries.count >= 2 else { return }
-        
-        // Set up xAxis' maximum value if the data range is less or equal to the latest trading time span.
-        let latestOpenTime = CalendarManager.shared.latestTradingTime.open.timeIntervalSince1970
-        let latestCloseTime = CalendarManager.shared.latestTradingTime.close.timeIntervalSince1970
-        let timeSpanOnTheLatestTradingTime = latestCloseTime - latestOpenTime
-        let isDataRangeWithinLatestTradingTimeRange = (entries.last!.x - entries.first!.x) <= timeSpanOnTheLatestTradingTime
-        
-        let startPrice = viewModel.data.first?.price ?? 0.0
-        let latestValue = viewModel.data.last?.price ?? 0.0
-        let valueChange = latestValue - startPrice
-        
-        // If the data range is within the time span of the latest trading time,
-        // the fill color of the line chart is determined by the difference
-        // between the previous close price and the last price in the data entry;
-        // Otherwise, it's determined by the difference between the first and the
-        // last price in the data entry.
-        var fillColor: UIColor = .stockPriceUp
-        if isDataRangeWithinLatestTradingTimeRange {
-            xAxis.axisMaximum = latestCloseTime
-            fillColor = (latestValue - viewModel.previousClose < 0) ? .stockPriceDown : .stockPriceUp
-        } else {
-            fillColor = valueChange < 0 ? .stockPriceDown : .stockPriceUp
-        }
-        
-        let dataSet = LineChartDataSet(entries: entries)
-        dataSet.drawCirclesEnabled = false
-        dataSet.drawIconsEnabled = false
-        dataSet.drawValuesEnabled = false
-        dataSet.drawFilledEnabled = true
-        dataSet.setColor(fillColor)
-        dataSet.fillColor = fillColor
-        dataSet.lineWidth = 1.5
-
-        let data = LineChartData(dataSet: dataSet)
-        self.data = data
+        setUpChartData(with: viewModel)
     }
     
     // MARK: - Private
@@ -104,15 +64,77 @@ class StockChartView: LineChartView {
         xAxis.avoidFirstLastClippingEnabled = true
         xAxis.granularity = 3600.0 // minimum interval between xAxis values
     }
+    
+    private func setUpChartData(with viewModel: ViewModel) {
+        // Chart Data Entries
+        let entries: [ChartDataEntry] = viewModel.data.map{
+            .init(x: $0.timeInterval, y: $0.price)
+        }
+        guard entries.count >= 2 else { return }
+        
+        let latestOpenTime = CalendarManager.shared.latestTradingTime.open.timeIntervalSince1970
+        let latestCloseTime = CalendarManager.shared.latestTradingTime.close.timeIntervalSince1970
+        let timeIntervalBetweenLatestTradingDay = latestCloseTime - latestOpenTime
+        guard let firstDataTimeStamp = entries.first?.x,
+              let lastDataTimeStamp = entries.last?.x else { return }
+        let isTimeRangeWithinLatestTradingTimeRange = (lastDataTimeStamp - firstDataTimeStamp) <= timeIntervalBetweenLatestTradingDay
+        
+        guard let startPrice = viewModel.data.first?.price,
+              let latestValue = viewModel.data.last?.price else { return }
+        let valueChange = latestValue - startPrice
+        
+        /*
+         Since the opening price of a stock could be higher or lower than its
+         previous close price, whether the price is higher or lower than the
+         previous close price cannot be determined by simply calculating the
+         difference between the first and the latest price, but the previous
+         close price and the latest price instead.
+         
+         If the data's time range is within the time span of the latest trading time,
+         the fill color of the line chart is determined by the difference
+         between the previous close price and the last price in the data entry;
+         Otherwise, it's determined by the difference between the first and the
+         last price in the data entry.
+         */
+        var fillColor: UIColor = .stockPriceUp
+        
+        // Set x-axis' maximum value if the data's time range is within the latest trading time span.
+        if isTimeRangeWithinLatestTradingTimeRange {
+            xAxis.axisMaximum = latestCloseTime
+            fillColor = (latestValue - viewModel.previousClose < 0) ? .stockPriceDown : .stockPriceUp
+        } else {
+            fillColor = valueChange < 0 ? .stockPriceDown : .stockPriceUp
+        }
+        
+        let dataSet = LineChartDataSet(entries: entries)
+        dataSet.drawCirclesEnabled = false
+        dataSet.drawIconsEnabled = false
+        dataSet.drawValuesEnabled = false
+        dataSet.drawFilledEnabled = true
+        dataSet.setColor(fillColor)
+        dataSet.lineWidth = 1.5
+        
+        // Set up gradient color fill.
+        let gradientColors = [fillColor.cgColor, UIColor.clear.cgColor] as CFArray
+        let gradientLocations: [CGFloat] = [1.0, 0.0]
+        if let gradient = CGGradient(colorsSpace: CGColorSpaceCreateDeviceRGB(),
+                                     colors: gradientColors,
+                                     locations: gradientLocations) {
+            dataSet.fill = LinearGradientFill(gradient: gradient, angle: 90.0)
+        }
+        
+        let data = LineChartData(dataSet: dataSet)
+        self.data = data
+    }
 
 }
 
 final class XAxisValueFormatter: AxisValueFormatter {
     func stringForValue( _ value: Double, axis _: AxisBase?) -> String {
         let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(identifier: "America/New_York")
         formatter.calendar = CalendarManager.shared.newYorkCalendar
         formatter.dateFormat = "h"
-        
         return formatter.string(from: Date(timeIntervalSince1970: value))
     }
 }
